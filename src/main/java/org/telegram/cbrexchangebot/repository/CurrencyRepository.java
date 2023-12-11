7db0b050ca8595ac857bc3fcb0a29e7150f685f8
Egor Yakovlev <riddik24@yandex.ru>
2023-12-12 00:37:32 +0300

    commit

--- src/main/java/org/telegram/cbrexchangebot/repository/CurrencyRepository.java
+++
@@ -1,9 +0,0 @@
-package org.telegram.cbrexchangebot.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import org.telegram.cbrexchangebot.model.Currency;
-
-@Repository
-public interface CurrencyRepository extends JpaRepository<Currency, Long> {
-}
